152ac6eab1c0edca9e99f642e102b7d374c02c25
alexa <alexa@PC_WINDOWS>
2025-01-21 14:19:24 -0500

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
